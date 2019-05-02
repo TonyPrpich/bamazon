8db82ccaf89309c5a1790879803f94206583cd6c
ArthurM0rgan <46456968+TonyPrpich@users.noreply.github.com>
2019-05-01 23:16:56 -0600

    swapped js with great bay js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,82 +1,121 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql");
+var inquirer = require("inquirer");
+var mysql = require("mysql");
 
-
-let itemInCart = {};
-let quantityOrdered = 0;
-
-
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  database: "bamazon",
-  password: "deezdeez",
-});
-
-const createOrder = () => {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "productOrdered",
-        message: "What is the ID number of the item you want to buy?"
-      },
-      {
-        type: "input",
-        name: "quantityOrdered",
-        message: "How many units do you want to buy?"
-      }
-    ])
-    .then(answers => {
-      connection.query(
-        "SELECT * FROM `products` WHERE `item_id` = ?",
-        [answers.productOrdered],
-        function(err, results) {
-          itemInCart = results[0];
-          quantityOrdered = answers.quantityOrdered;
-          itemInCart.stock_quantity >= quantityOrdered
-            ? fulfillOrder(itemInCart, quantityOrdered)
-            : (console.log("Insufficient quantity!"), connection.close());
-        }
-      );
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
-
-const fulfillOrder = (itemInCart, quantityOrdered) => {
-  connection.query(
-    "UPDATE `products` SET product_sales = product_sales + ?, ? WHERE ?",
-    [
-      quantityOrdered * itemInCart.price,
-      { stock_quantity: itemInCart.stock_quantity - quantityOrdered },
-      { item_id: itemInCart.item_id }
-    ],
-    function(err, results) {
-      console.log(results);
-      console.log(
-        `You just bought ${quantityOrdered} unit(s) of ${
-          itemInCart.product_name
-        }. Your total is $${itemInCart.price * quantityOrdered}. 
-          Thank you for shopping with Bamazon!`
-      );
-    }
-  );
-  connection.close();
-};
-
-
-connection.query("SELECT * FROM `products` WHERE deleted is null", function(
-  err,
-  results
-) {
-  console.log("Items for sale today at Bamazon:\n");
-  results.forEach(result => {
-    console.log(
-      `Item ${result.item_id} - ${result.product_name} - $${result.price}`
-    );
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: "deezdeez",
+    database: "bamazon"
   });
-  createOrder();
-});
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    start()
+    })
+
+  function start() {
+  inquirer.prompt ([
+    {
+        type: "list",
+        name: "initialOption",
+        message: "What would you like to do?",
+        choices: ["Post an Item", "Bid on an Item", "Exit"]
+      }
+]).then(function(answer){
+    console.log(answer.initialOption);
+    if (answer.initialOption === "Post an Item") {
+        postItem();
+    } else if (answer.initialOption === "Bid on an Item") {
+        bidItem();
+    } else {
+        console.log("Goodbye")
+        connection.end();
+    }
+})
+  } 
+  function postItem() {
+        inquirer.prompt ([
+            {
+                type: "input",
+                name: "item",
+                message: "What item would you like to sell?"
+            },
+            {
+                type: "input",
+                name: "description",
+                message: "Please describe the item you are selling:"
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "What is the starting price for your item?"
+            }
+        ]).then(function(answers){
+                console.log("Creating a new listing...\n");
+                connection.query(
+                  "INSERT INTO items SET ?",
+                  {
+                    item: answers.item,
+                    description: answers.description,
+                    StartingBid: answers.price,
+                    CurrentBid: answers.price
+                  },
+                  function(err, res) {
+                      if (err) throw err;
+                    console.log(res.affectedRows + " item inserted!\n");
+                    start();
+                  }
+                )} 
+        )}
+
+        function bidItem() {
+            var itemsArray = [];
+            connection.query("SELECT * FROM items", function (err, res) {
+                if(err) throw err;
+                for(var i = 0; i < res.length; i++) {
+                    itemsArray.push(res[i].id.toString())
+                    console.log(" ID #: " + res[i].id + " Item: " + res[i].item + " Description: " + res[i].description + " Starting Bid: " + res[i].StartingBid + " Current Bid: " + res[i].CurrentBid)
+                }
+                inquirer.prompt ([
+                    {
+                        type: "list",
+                        name: "itemSelect",
+                        message: "What item would you like to bid on?",
+                        choices: itemsArray        
+                    }, 
+                    {
+                        type: "input",
+                        name: "bidAmount",
+                        message: "How much would you like to bid? "
+                    }
+
+                ]).then(function (response){
+                    var userChoice = res.find(o => o.id == response.itemSelect);
+                    if (userChoice.CurrentBid < response.bidAmount) {
+                    connection.query(
+                        "UPDATE items set ? where ?",
+                        [{
+                          CurrentBid: response.bidAmount
+                        },
+                        {
+                          id: response.itemSelect
+                        }]
+
+                        
+                ); console.log("Bid Successful!")
+                start();
+                    } else {
+                        console.log("Please enter a higher bid.")
+                        bidItem();
+                    }
+            }) 
+            }) 
+        }
